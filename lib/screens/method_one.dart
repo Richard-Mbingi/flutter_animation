1ef7452ae9ad1286b821b72be1cb36f89c87e70b
Richard Mbingi <mbingicoding@gmail.com>
2021-02-09 19:05:04 +0300

    Method one animation

--- lib/screens/method_one.dart
+++ lib/screens/method_one.dart
@@ -1,14 +1,50 @@
+import 'dart:math' show pi;
 import 'package:flutter/material.dart';
 
-class MethodOne extends StatelessWidget {
-  const MethodOne({Key key}) : super(key: key);
+class MethodOne extends StatefulWidget {
+  _MethodOneState createState() => _MethodOneState();
+}
+
+class _MethodOneState extends State<MethodOne>
+    with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 5),
+    );
+
+    animation = Tween<double>(
+      begin: 0,
+      end: 2 * pi,
+    ).animate(animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Method 1'),
+      body: Transform.rotate(
+        angle: animation.value,
+        child: Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(30),
+            child: Image.asset('assets/images/logo.png')),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 }
